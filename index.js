1e97a7103723901d22020a932fcc3f92ef653c22
Hack Reactor Student <student@hackreactor.com>
2017-09-17 14:29:28 -0700

    Render first react component.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const app = express();
-const db = require('./server/db');
 
+const path = require('path');
+const db = require('./db/db');
+
+
+const app = express();
 console.log('what is db', db);
 
+// app.get('/', (req, res) => {
+//   db.User.findAll().then(users => {
+//     console.log('USERS', users);
+//   });
+//   db.othertable.findAll().then(others => {
+//     console.log('OTHERS', others);
+//   });
+//   res.send('hello world?');
+// });
 
-app.get('/', function (req, res) {
-  db.User.findAll().then(users => {
-    console.log('USERS', users);
-  });
-  db.othertable.findAll().then(others => {
-    console.log('OTHERS', others);
-  });
-  res.send('hello world?');
-});
+app.use(express.static(path.join(__dirname, '/dist')));
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('example app listening on port 3000');
 });
